87708d4d36c5eca7178e64f4aeed6a029aa68f15
Justin Lam <justin.kh.lam@gmail.com>
2021-10-09 20:37:00 +0000

    add logic to queries 1-3

---
+++ 1_queries/01_user_login.sql
@@ -0,0 +1,7 @@
+SELECT users.id as id, users.name as name, users.email as email, users.password as passwords
+from users
+WHERE users.email = 'tristanjacobs@gmail.com';
+
+-- SELECT id, name, email, password
+-- FROM users
+-- WHERE email = 'tristanjacobs@gmail.com';
