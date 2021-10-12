08a965939b89cc1991b123059707a82f8f759b56
Justin Lam <justin.kh.lam@gmail.com>
2021-10-12 18:28:25 +0000

    add comments for query 5

--- 1_queries/05_all_reservations.sql
+++ 1_queries/05_all_reservations.sql
@@ -7,3 +7,8 @@ AND reservations.end_date < now()::date
 GROUP BY properties.id, reservations.id
 ORDER BY reservations.start_date
 LIMIT 10;
+
+-- FROM reservations since it's the one we're basing this on
+-- Join properties with reservations.property_id so that it's linked to properties.id and we can access all columns that way
+-- Same with property_reviews
+-- Group by so that we get all the aggregated properties.id/res.id, otherwise it'll show all for each one
