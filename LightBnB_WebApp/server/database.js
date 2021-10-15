1d870dc2e8d9be86ed46968f13ee7f21447fb897
Justin Lam <justin.kh.lam@gmail.com>
2021-10-15 04:51:12 +0000

    add logic for getAllReservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -105,8 +105,13 @@ exports.addUser = addUser;
 const getAllReservations = function(guest_id, limit = 10) {
   return pool
     .query(`
-    SELECT * FROM reservations
-    WHERE guest_id = $1
+    SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date desc
     LIMIT $2;
     `, [guest_id, limit])
     .then((result) => {
