f1cda9c8e489c2d7f5543d9c933131809eca9760
Justin Lam <justin.kh.lam@gmail.com>
2021-10-16 05:56:17 +0000

    add code to display Reserve button in property_listing.js, add event listener for the Reserve button in index.js

--- LightBnB_WebApp/public/javascript/index.js
+++ LightBnB_WebApp/public/javascript/index.js
@@ -2,5 +2,11 @@ $(() => {
   getAllListings().then(function( json ) {
     propertyListings.addProperties(json.properties);
     views_manager.show('listings');
+    // on-click event listener for reserve button
+    // when clicked, callback function engages
+    $('.reserve-button').on('click', function() {
+      const idData = $(this).attr('id').substring(17);
+      views_manager.show('newReservation', idData);
+    })
   });
 });
